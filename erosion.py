caffa355463f35ce86e6cb92e13e8fdf798b45af
hugoSauvage <126684588+hugoSauvage@users.noreply.github.com>
2026-01-05 11:09:31 +0100

    testePhase1
    Co-Authored-By: Nathan Fasquel Willard <191609409+FASQUELWILLARDNathan@users.noreply.github.com>
    Co-Authored-By: Clément Dequidt <204934912+ClemTeuf@users.noreply.github.com>

--- erosion.py
+++ erosion.py
@@ -46,7 +46,7 @@ else:
 # Define a kernel for erosion
 kernel = np.ones((3,3), np.uint8)
 # Perform erosion
-eroded_image = cv.erode(image, kernel, iterations=1)
+eroded_image = cv.erode(image, kernel, iterations=5)
 
 # Save the eroded image 
 cv.imwrite('./results/eroded.png', eroded_image)
